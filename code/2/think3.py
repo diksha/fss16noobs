733a08fd47002645a3b2334cf6daf2e0ff208a20
Rajat Shah <rajat.shah200792@gmail.com>
2016-08-30 00:18:01 +0000

    ThinkPython exercise 3 partial solution

---
+++ code/2/think3.py
@@ -0,0 +1,60 @@
+# Exercise 3.1, 3.2
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+repeat_lyrics()
+
+# Exercise 3.3
+
+def right_justify(s):
+    assert len(s) <= 70, "More than 70 chars in string" 
+    print ' ' * (70 - len(s)) + s
+
+right_justify('allen')
+
+# Exercise 3.4
+
+def do_twice(f, v):
+    f(v)
+    f(v)
+
+def print_twice(s):
+    print s
+    print s
+
+def do_four(f, v):
+    do_twice(f, v)
+    do_twice(f, v)
+
+do_twice(print_twice, 'spam')
+
+do_four(print_twice, 'spam')
+
+# Exercise 3.5, Part 1
+
+def do_Ntimes(f, n = 1):
+    for i in xrange(n):
+        f()
+
+def draw_border():
+    print '+', " ".join("-"*4), '+', " ".join("-"*4), '+'
+
+def draw_row():
+    print '|', " "*7, '|', " "*7, '|'
+
+def draw_grid():
+    do_Ntimes(draw_border, 1)
+    do_Ntimes(draw_row, 4)
+    do_Ntimes(draw_border, 1)
+    do_Ntimes(draw_row, 4)
+    do_Ntimes(draw_border, 1)
+    
+draw_grid()
+
+# Exercise 3.5, Part 2
