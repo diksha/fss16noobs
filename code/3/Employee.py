ede05a9e6c9b5841c4bd8287e53cdc12fbdf45d0
Rajat Shah <rajat.shah200792@gmail.com>
2016-09-08 01:09:02 +0000

    Adding HW3 Employee class

---
+++ code/3/Employee.py
@@ -0,0 +1,19 @@
+from __future__ import division
+import sys,random
+sys.dont_write_bytecode=True
+
+class Employee:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __repr__(self):
+        return ("{name: "+self.name+", age: "+str(self.age)+"}")
+    
+    def __lt__(self, e):
+        return self.age < e.age
+
+print Employee("Alex", 34)
+
+employees = [Employee("Alex", 32), Employee("Bob", 23), Employee("Jane", 27)]
+print sorted(employees)
